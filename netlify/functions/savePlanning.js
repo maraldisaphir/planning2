758b7a0d310b5e0a515209c3451eb63bad9fda0b
maraldisaphir <michael.maraldi@gmail.com>
2025-09-04 18:37:39 +0200

    Add files via upload

--- netlify/functions/savePlanning.js
+++ netlify/functions/savePlanning.js
@@ -1,8 +1,8 @@
 const { getStore } = require("@netlify/blobs");
 
 function store(){
-  const siteID = process.env.NETLIFY_SITE_ID;
-  const token  = process.env.NETLIFY_API_TOKEN;
+  const siteID = process.env.cc36154d-c4e0-4ede-9976-d91a0bb9b9c8;
+  const token  = process.env.nfp_JgSejZWAKbsnsCCG72Xpr6vq5CFW6A5e22a4;
   if(!siteID || !token){
     throw new Error("Missing NETLIFY_SITE_ID or NETLIFY_API_TOKEN in environment variables.");
   }
